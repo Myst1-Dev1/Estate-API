75a3818ebeed6e7d4b8caf742792f85cfd1fa883
João Victor <joao.victor@internit.net.br>
2025-03-06 00:05:57 -0300

    more added on api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import postRoute from './routes/post.route.js';
 import authRoute from './routes/auth.route.js';
 import testRoute from './routes/test.route.js';
+import userRoute from './routes/user.route.js';
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json()); // Utilizado para que a aplicação possa enviar conte�
 app.use(cookieParser());
 
 
+app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/test", testRoute);
--- routes/post.route.js
+++ routes/post.route.js
@@ -1,21 +1,13 @@
 import express from 'express';
+import { verifyToken } from '../middleware/verifyToken.js';
+import { addPosts, deletePost, getPost, getPosts, updatePosts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
-// router.get("/test", (req, res) => {
-//     console.log("Router works");
-// });
-
-// router.post("/test", (req, res) => {
-//     console.log("Router works");
-// });
-
-// router.put("/test", (req, res) => {
-//     console.log("Router works");
-// });
-
-// router.delete("/test", (req, res) => {
-//     console.log("Router works");
-// });
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", verifyToken, addPosts);
+router.post("/:id", verifyToken, updatePosts);
+router.delete("/:id", verifyToken, deletePost);
 
 export default router;
