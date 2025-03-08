adacffde18c31e397f09bb23545381529965ccf8
João Victor <joao.victor@internit.net.br>
2025-03-08 00:46:04 -0300

    more on api

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ import postRoute from './routes/post.route.js';
 import authRoute from './routes/auth.route.js';
 import testRoute from './routes/test.route.js';
 import userRoute from './routes/user.route.js';
+import chatRoute from './routes/chat.route.js';
+import messageRoute from './routes/message.route.js';
 
 const app = express();
 
-app.use(cors({origin:process.env.CLIENT_URL, credentials:true}));
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json()); // Utilizado para que a aplicação possa enviar conteúdo como JSON
 app.use(cookieParser());
 
@@ -17,6 +23,8 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(8800, () => {
     console.log('Server is running!');
